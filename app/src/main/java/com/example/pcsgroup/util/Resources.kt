b8c114b83e818678833efc1e1590f7ec8de0ea93
muhammadaqilp <haimac@Aqils-MacBook-Pro.local>
2024-08-26 21:08:43 +0700

    create sealed class resources

--- app/src/main/java/com/example/pcsgroup/util/Resources.kt
+++ app/src/main/java/com/example/pcsgroup/util/Resources.kt
@@ -3,5 +3,6 @@ package com.example.pcsgroup.util
 sealed class Resources<T>(val data: T? = null, val throwable: Throwable? = null) {
     class Success<T>(data: T) : Resources<T>(data)
     class Loading<T>(data: T? = null) : Resources<T>(data)
+    class Empty<T>(data: T? = null) : Resources<T>(data)
     class Error<T>(throwable: Throwable?) : Resources<T>(throwable = throwable)
 }
