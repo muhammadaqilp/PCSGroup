e8f07a96eb7d5cdf1922dded2c67a2f621bc4c71
muhammadaqilp <haimac@Aqils-MacBook-Pro.local>
2024-08-26 21:09:00 +0700

    create Constant class

---
+++ app/src/main/java/com/example/pcsgroup/util/Constant.kt
@@ -0,0 +1,13 @@
+package com.example.pcsgroup.util
+
+object Constant {
+
+    object ReflectionClassName {
+        const val DETAILS_CLASS_NAME = "com.example.details.ui.detail.DetailActivity"
+    }
+
+    object Intent {
+        const val EXTRA_DETAIL_DATA = "EXTRA_DETAIL_DATA"
+    }
+
+}
